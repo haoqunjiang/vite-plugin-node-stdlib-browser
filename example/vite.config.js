731c979998259035db1f86e5b406f51131d0c529
Haoqun Jiang <haoqunjiang@gmail.com>
2023-03-06 15:39:09 +0800

    chore: add vite-plugin-inspect to the example

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import nodePolyfills from 'vite-plugin-node-stdlib-browser'
+import inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [nodePolyfills()],
+  plugins: [
+    nodePolyfills(),
+    inspect({
+      build: true,
+      outputDir: '.vite-inspect'
+    })
+  ],
   optimizeDeps: {
     include: ['test-fixture']
   },
   build: {
     commonjsOptions: {
       include: [/test-fixture/, /node_modules/]
-    }
+    },
+    minify: false
   }
 })
